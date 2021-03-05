e9a2b3704371bba0c65f5d245199b6c9ad86402a
Don Denton <don@happycollision.com>
2021-03-04 21:46:46 -0500

    -- WIP correct config intellisense

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { resolve } from 'path';
 
 // Consult https://vitejs.dev/config/ to learn about these options
-/** @type {import("vite").ResolvedConfig } */
-export default {
-	resolve: {
-		alias: {
-			$components: resolve('src/components'),
-			$app: resolve('.svelte/assets/runtime/app')
-		}
-	},
-	build: {
-		sourcemap: true
-	}
+/** @type {import("vite").UserConfig } */
+const config = {
+    resolve: {
+        alias: {
+            $components: resolve('src/components'),
+            $app: resolve('.svelte/assets/runtime/app')
+        }
+    },
+    build: {
+        sourcemap: true
+    }
 };
+
+export default config

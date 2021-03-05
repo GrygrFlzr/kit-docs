ed10fb42cf365039603f0a1310aa92123b4c794d
Don Denton <don@happycollision.com>
2021-03-04 21:31:00 -0500

    -- WIP
    This should eventually be squashed into the previous commit. For now,
    these changes *seem* correct, though I still cannot run the app.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-// Consult https://vitejs.dev/config/ to learn about these options
 import { resolve } from 'path';
 
+// Consult https://vitejs.dev/config/ to learn about these options
+/** @type {import("vite").ResolvedConfig } */
 export default {
 	resolve: {
 		alias: {
-			$components: resolve('src/components')
+			$components: resolve('src/components'),
+			$app: resolve('.svelte/assets/runtime/app')
 		}
+	},
+	build: {
+		sourcemap: true
 	}
 };
